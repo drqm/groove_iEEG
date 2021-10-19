74745bda5309d4b8e4e11f2096aac4c5e2221830
drqm <dquiroga@clin.au.dk>
2021-10-19 15:15:56 +0200

    added serial port option

--- scripts/triggers.py
+++ scripts/triggers.py
@@ -1,22 +1,31 @@
 #54121 -+sycu 6y 
 from psychopy import parallel
+import serial
 
-# Set port address to match your local machine
+##### Set port address to match your local machine
+
 paddress = '/dev/parport0' # '0xDFF8' in Windows
-try:
-    port = parallel.setPortAddress(address=paddress) 
-except NotImplementedError:
-    port = []
-# NB problems getting parallel port working under conda env
-# from psychopy.parallel._inpout32 import PParallelInpOut32
-# port = PParallelInpOut32(address=0xDFF8)  # on MEG stim PC
-# parallel.setPortAddress(address='0xDFF8')
-# port = parallel
 
-# Figure out whether to flip pins or fake it
-if port:
+##################################################
+
+try:
+    port = serial.Serial(paddress)
+    port_type = 'serial'
+except NotImplementedError:
+    port = parallel.setPortAddress(address=paddress)
+    port_type = 'parallel'
+except:
+    port_type = 'Not set'
+
+print('port type: {}'.format(port_type))
+
+if port_type == 'parallel':
     def setParallelData(code=1):
         port.setData(code)
+        print('trigger sent {}'.format(code))
+elif port_type == 'serial':
+    def setParallelData(code=1):
+        port.write(code)
         print('trigger sent {}'.format(code))
 else:
     def setParallelData(code=1):
